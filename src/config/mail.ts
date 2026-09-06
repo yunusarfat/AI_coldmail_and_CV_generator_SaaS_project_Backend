feedef6ead8e68de1fd6163082b2a3f3f4716f4e
MOHAMMAD ARFAT <mdarfatyunus@gmail.com>
2026-09-06 12:31:57 +0600

    add mail

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -1,10 +1,25 @@
+// import nodemailer from "nodemailer";
+// export const transporter=nodemailer.createTransport(
+//     {
+//         service:"gmail",
+//         auth:{
+//             user:process.env.EMAIL_USER,
+//             pass:process.env.EMAIL_PASS,
+//         }
+//     }
+// )
+
+
+
 import nodemailer from "nodemailer";
-export const transporter=nodemailer.createTransport(
-    {
-        service:"gmail",
-        auth:{
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASS,
-        }
-    }
-)
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4, // 👈 force IPv4, skip broken IPv6 route
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+} as nodemailer.TransportOptions); // 👈 Added type casting here
