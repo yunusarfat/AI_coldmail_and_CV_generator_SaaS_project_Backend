e42c2d1bab53aefce0403b7d375f9c6689ef5700
MOHAMMAD ARFAT <mdarfatyunus@gmail.com>
2026-09-06 13:02:17 +0600

    add false

--- src/config/mail.ts
+++ src/config/mail.ts
@@ -16,10 +16,15 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: true,
+  secure: false,
   family: 4, // 👈 force IPv4, skip broken IPv6 route
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+   tls: {
+    rejectUnauthorized: false, // 👈 Prevents Render from dropping the connection during the TLS handshake
+    ciphers: 'SSLv3'
+  }
+
 } as nodemailer.TransportOptions); // 👈 Added type casting here
